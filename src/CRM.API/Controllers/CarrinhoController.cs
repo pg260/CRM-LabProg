1cef7ae5ecd665851c945e4fc1fd9c2caf5c7e65
pg260 <spg2616@gmail.com>
2023-10-29 17:05:57 -0300

    Achoque foi

---
+++ src/CRM.API/Controllers/CarrinhoController.cs
@@ -0,0 +1,45 @@
+﻿using CRM.API.Responses;
+using CRM.Service.Contracts;
+using CRM.Service.Dtos.ProdutoCarrinhoDtos;
+using CRM.Service.NotificatorConfig;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+namespace CRM.API.Controllers;
+
+[Microsoft.AspNetCore.Components.Route("v1/Carrinho")]
+public class CarrinhoController : BaseController
+{
+    public CarrinhoController(INotificator notificator, ICarrinhoService carrinhoService) : base(notificator)
+    {
+        _carrinhoService = carrinhoService;
+    }
+
+    private readonly ICarrinhoService _carrinhoService;
+    
+    [HttpPut("{id}")]
+    [SwaggerOperation(Summary = "Adiciona um produto.", Tags = new[] { "Carrinho" })]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(BadRequestResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> AdicionarProduto(int id, [FromBody] AlterarProdutoCarrinhoDto dto)
+    {
+        await _carrinhoService.AdicionarProduto(id, dto);
+        return NoContentResponse();
+    }
+    
+    [HttpDelete("{id}")]
+    [SwaggerOperation(Summary = "Remove um produto.", Tags = new[] { "Carrinho" })]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(BadRequestResponse), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> RemoverProduto(int id, [FromBody] AlterarProdutoCarrinhoDto dto)
+    {
+        await _carrinhoService.RemoverProduto(id, dto);
+        return NoContentResponse();
+    }
+}
